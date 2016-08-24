af09102ead0b7681c13038e827a6b02bc5bcfe6b
tarunchhabra26 <tarun.chhabra@gmail.com>
2016-08-23 20:49:19 -0400

    Made changes to main.py and README.md

--- code/1/main.py
+++ code/1/main.py
@@ -3,7 +3,7 @@ main.py (c) 2016 tarunchhabra.com, Apache licence
 
 main.py to test utest.py program
 """
-import utest
+from utest import ok,oks
 import who1
 import who2
 import who3
@@ -13,24 +13,27 @@ from fib import fib
 def is_equal(x,y):
 	return x==y
 
-@utest.ok
+@ok
 def fail_test():
 	"Adding another test to check results"
 	assert 1 == 2
-@utest.ok
+@ok
 def get_fib5():
 	"To get the fibonacci number for 5th term in series"
 	number = fib(5)
 	print '5th term is fibonacci series is : %d' % number
 	assert 5 == number
-@utest.ok
+@ok
 def get_fib100():
 	"To get the fibonacci number for 100th term in series"
 	number = fib(100)
 	print '100th term is fibonacci series is : %d' % number
 	assert number == 354224848179261915075
-@utest.ok
+@ok
 def get_fib3():
 	number = fib(3)
 	print '3rd term in the fibonacci series is : %d' % number
 	assert number == 2
+
+oks()
+
