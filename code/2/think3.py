0aeb3f7bdc15fcd332231b6fc433eada8927ba9e
tarunchhabra26 <tarun.chhabra@gmail.com>
2016-08-27 12:49:12 -0400

    Added think3.py and think4.py for homework 2

---
+++ code/2/think3.py
@@ -0,0 +1,7 @@
+"""
+Exercise questions from Think like a Computer Scientist
+http://www.greenteapress.com/thinkpython/html/index.html
+
+Exercises 3.1,3.2,3.3,3.4,3.5
+
+"""
---
+++ code/2/think4.py
@@ -0,0 +1,7 @@
+"""
+Exercise questions from Think like a Computer Scientist
+http://www.greenteapress.com/thinkpython/html/index.html
+
+Exercises 4.2,4.3
+
+"""
