de19954e210da7cf319a22c7af60a1969eb515fa
sbiswas4 <sbiswas4@ncsu.edu>
2016-09-16 23:44:22 -0400

    Simulated annealer code updated 1

--- code/4/simann.py
+++ code/4/simann.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import random
 import math
 
@@ -7,44 +8,40 @@ def schaff(s):
     eschaff = f1 + f2
     return eschaff
 
-def schaffminmax():
-    run = 0
-    max=0.0
-    min=0.0
-    for run in range(1, 101):
-        schaff_input = random.randint(1,100)
-        eschaff=schaff(schaff_input)
-        if max < eschaff:
-            max = eschaff
-        if min > eschaff:
-            min = eschaff
-    return (min, max)
+def min_max():
+    values = []
+    for i in range(100):
+        values.append(schaff(random.randint(-100000, 100000)))
+    mini = min(values)
+    maxi = max(values)
+    return (mini,maxi)
+    # print("min:{} max:{}".format(mini, maxi))
 
 def energyfunc(s,min,max):
     schaff_val=schaff(s)
     norm_schaff=(schaff_val-min)/(max-min)
     return  norm_schaff
 
-
-
 def neighbour(s):
-    sn= random.uniform(s+1.1,s+0.9)
+    sn= random.uniform(s*0.9, s*1.1)
     return sn
 
 def crazyjump(e,en,t):
-    jmp_prob=math.exp((en-e)/t)
+    jmp_prob=math.exp((e-en)/t)
+    return jmp_prob
 
-def simannler():
-    state= random.randint((-10)**5,(10)**5)
-    k=0.0
+def simannealer():
+    state= random.randint(-100000,100000)
+    k=1
     kmax=1000
     emax=-1
-    scahff_minmax = schaffminmax()
+    scahff_minmax = min_max()
     min = scahff_minmax[0]
     max = scahff_minmax[1]
     energy = energyfunc(state,min,max)
     beststate = state
     bestenergy = energy
+    print("\n, %04d, :%3.5f " % (k, bestenergy), end="")
     while k<kmax and energy>emax:
         nextstate=neighbour(state)
         nextenergy=energyfunc(nextstate,min, max)
@@ -60,12 +57,21 @@ def simannler():
 
         elif nextenergy>energy:
             prob_crazyjump=crazyjump(energy,nextenergy, k/kmax)
-            if prob_crazyjump<random.randint():
+            if prob_crazyjump<random.randint(0,1):
                 state=nextstate
                 energy=nextenergy
                 print("?", end="")
-        print(".")
+        print(".", end="")
         k=k+1
-        if k%50==0:
-             print("\n")
-             print(beststate)
+        if k%25==0:
+            print("\n, %04d, :%3.5f " % (k, bestenergy), end="")
+    print("\n")
+    print("beststate:{}".format(beststate))
+    print("k:{}".format(k))
+    print("bestenergy:{}".format(bestenergy))
+
+
+
+simannealer()
+
+
