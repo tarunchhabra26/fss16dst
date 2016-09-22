b40a928d2a80ff69d2e2bc614f0750e41f2f89a8
sbiswas4 <sbiswas4@ncsu.edu>
2016-09-21 22:58:22 -0400

    Updated with comments

--- code/4/simann.py
+++ code/4/simann.py
@@ -2,13 +2,13 @@ from __future__ import print_function
 import random
 import math
 
-def schaff(s):
+def schaff(s):                                                                  #Scahffer function
     f1 = math.pow(s, 2)
     f2 = math.pow((s - 2), 2)
     eschaff = f1 + f2
     return eschaff
 
-def min_max():
+def min_max():                                                                  #calc min & max Schaffer for Normalization
     values = []
     for i in range(100):
         values.append(schaff(random.randint(-100000, 100000)))
@@ -17,45 +17,45 @@ def min_max():
     return (mini,maxi)
     # print("min:{} max:{}".format(mini, maxi))
 
-def energyfunc(s,min,max):
+def energyfunc(s,min,max):                                                      #calculate normalized Schaffer of a state
     schaff_val=schaff(s)
     norm_schaff=(schaff_val-min)/(max-min)
     return  norm_schaff
 
-def neighbour(s):
+def neighbour(s):                                                               #pick next state
     sn= random.uniform(s*0.9, s*1.1)
     return sn
 
-def crazyjump(e,en,t):
+def crazyjump(e,en,t):                                                          #calculate probability of crazy jump
     jmp_prob=math.exp((e-en)/t)
     return jmp_prob
 
 def simannealer():
     state= random.randint(-100000,100000)
-    k=1
+    k=1                                                                         #initializations
     kmax=1000
     emax=-1
-    scahff_minmax = min_max()
-    min = scahff_minmax[0]
+    scahff_minmax = min_max()                                                   #Schaffer baseline max & min
+    min = scahff_minmax[0]                                                      
     max = scahff_minmax[1]
-    energy = energyfunc(state,min,max)
+    energy = energyfunc(state,min,max)                                          #initial state energy
     beststate = state
     bestenergy = energy
     print("\n, %04d, :%3.5f " % (k, bestenergy), end="")
-    while k<kmax and energy>emax:
+    while k<kmax and energy>emax:                                               #loop invariant
         nextstate=neighbour(state)
         nextenergy=energyfunc(nextstate,min, max)
-        if nextenergy<bestenergy:
+        if nextenergy<bestenergy:                                               #make next state and next energy as the best state and best enrgy
             beststate=nextstate
             bestenergy=nextenergy
             print('!',end="")
 
-        if nextenergy<energy:
+        if nextenergy<energy:                                                   #make next state and next energy as the current state and current energy
             state=nextstate
             energy=nextenergy
             print('+',end="")
 
-        elif nextenergy>energy:
+        elif nextenergy>energy:                                                 #decision on crazy jump 
             prob_crazyjump=crazyjump(energy,nextenergy, k/kmax)
             if prob_crazyjump<random.randint(0,1):
                 state=nextstate
@@ -65,7 +65,7 @@ def simannealer():
         k=k+1
         if k%25==0:
             print("\n, %04d, :%3.5f " % (k, bestenergy), end="")
-    print("\n")
+    print("\n")                                                                 #report end state and end energy
     print("beststate:{}".format(beststate))
     print("k:{}".format(k))
     print("bestenergy:{}".format(bestenergy))
